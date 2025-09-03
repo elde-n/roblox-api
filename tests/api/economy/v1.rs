7538e69fb6a614c6badb71ba1ad638855b27b725
elden <git@elden.anonaddy.com>
2025-09-03 18:26:33 +0300

    test(economy::v1): update remove purchase api test - seems to be disabled add currency api for user tests

--- tests/api/economy/v1.rs
+++ tests/api/economy/v1.rs
@@ -1,21 +1,26 @@
 use dotenvy_macro::dotenv;
 
-use roblox_api::{Currency, api::economy, client::Client};
+use roblox_api::{
+    api::{economy, users},
+    client::Client,
+};
 
 #[tokio::test]
-async fn purchase() {
+async fn currency() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+
+    client.ensure_token().await.unwrap();
+    economy::v1::currency(&mut client).await.unwrap();
+}
+
+#[tokio::test]
+async fn currency_from_user_id() {
     let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
 
     client.ensure_token().await.unwrap();
 
-    let asset_details = economy::v2::details(&mut client, 48474313).await.unwrap();
-    economy::v1::purchase(
-        &mut client,
-        asset_details.product_id,
-        0,
-        Currency::default(),
-        Some(asset_details.creator.id),
-    )
-    .await
-    .unwrap();
+    let authenticated = users::v1::authenticated_details(&mut client).await.unwrap();
+    economy::v1::currency_from_user_id(&mut client, authenticated.id)
+        .await
+        .unwrap();
 }
