414ecd40be2952a73da1da92320a06b91616c8e3
elden <git@elden.anonaddy.com>
2025-09-03 18:27:58 +0300

    test(users::v1): move `examples/users` to tests

--- tests/api/users/v1.rs
+++ tests/api/users/v1.rs
@@ -1,5 +1,5 @@
 use dotenvy_macro::dotenv;
-use roblox_api::{api::users, client::Client};
+use roblox_api::{Paging, SortOrder, api::users, client::Client};
 
 #[tokio::test]
 async fn authenticated_details() {
@@ -40,6 +40,48 @@ async fn validate_display_name_by_id() {
 
     let authenticated = users::v1::authenticated_details(&mut client).await.unwrap();
     users::v1::validate_display_name_by_id(&mut client, authenticated.id, "エルデン")
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn user_details() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+
+    client.ensure_token().await.unwrap();
+    users::v1::user_details(&mut client, 1).await.unwrap();
+}
+
+#[tokio::test]
+async fn user_username_history() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+
+    client.ensure_token().await.unwrap();
+    users::v1::user_username_history(
+        &mut client,
+        85382088,
+        Paging::new(None, Some(100), Some(SortOrder::Ascending)),
+    )
+    .await
+    .unwrap();
+}
+
+#[tokio::test]
+async fn users_by_id() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+
+    client.ensure_token().await.unwrap();
+    users::v1::users_by_id(&mut client, &[1, 2, 3, 4], false)
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn users_by_name() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+
+    client.ensure_token().await.unwrap();
+    users::v1::users_by_name(&mut client, &["Roblox", "test", "word"], false)
         .await
         .unwrap();
 }
