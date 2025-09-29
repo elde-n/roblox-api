9a7de79397f34f92727820bacb967a125afd39a3
elden <git@elden.anonaddy.com>
2025-09-30 00:16:06 +0300

    feat(friends::v1): extend api

--- src/api/friends/v1.rs
+++ src/api/friends/v1.rs
@@ -1,7 +1,7 @@
 use reqwest::Method;
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
 
-use crate::{Error, client::Client};
+use crate::{DateTime, Error, Paging, client::Client};
 
 pub const URL: &str = "https://friends.roblox.com/v1";
 
@@ -14,11 +14,116 @@ pub struct FollowingStatus {
     pub is_followed: bool,
 }
 
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+pub struct FriendStatus {
+    pub id: u64,
+    pub status: String,
+}
+
+#[derive(Copy, Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+pub enum FriendRequestSourceType {
+    InGame,
+    UserProfile,
+    PlayerSearch,
+    FriendRecommendations,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct FriendRequester {
+    #[serde(rename = "senderId")]
+    pub id: u64,
+    #[serde(rename = "senderNickname")]
+    pub display_name: String,
+    pub contact_name: Option<String>,
+
+    pub source_universe_id: u64,
+    pub origin_source_type: FriendRequestSourceType,
+    pub sent_at: DateTime,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct FriendRequest {
+    pub id: u64,
+    pub mutual_friends_list: Vec<String>,
+    #[serde(rename = "friendRequest")]
+    pub requester: FriendRequester,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct FriendRequests {
+    #[serde(rename = "data")]
+    pub requests: Vec<FriendRequest>,
+    #[serde(rename = "nextPageCursor")]
+    pub next_cursor: Option<String>,
+    #[serde(rename = "previousPageCursor")]
+    pub previous_cursor: Option<String>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct User {
+    pub id: u64,
+    #[serde(rename = "hasVerifiedBadge")]
+    pub is_verified: Option<bool>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct Followers {
+    #[serde(rename = "data")]
+    pub users: Vec<User>,
+    #[serde(rename = "nextPageCursor")]
+    pub next_cursor: Option<String>,
+    #[serde(rename = "previousPageCursor")]
+    pub previous_cursor: Option<String>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "PascalCase")]
+pub struct FriendsFind {
+    #[serde(rename = "PageItems")]
+    pub users: Vec<User>,
+    pub next_cursor: Option<String>,
+    pub previous_cursor: Option<String>,
+    pub has_more: Option<bool>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct UserPresence {
+    #[serde(rename = "UserPresenceType")]
+    pub kind: String,
+    #[serde(rename = "UserLocationType")]
+    pub location_kind: String,
+
+    #[serde(rename = "lastLocation")]
+    pub status: String,
+    pub last_online: DateTime,
+
+    pub place_id: Option<u64>,
+    pub root_place_id: Option<u64>,
+    pub universe_id: Option<u64>,
+    #[serde(rename = "gameInstanceId")]
+    pub job_id: Option<String>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
+#[serde(rename_all = "camelCase")]
+pub struct FriendOnlineStatus {
+    pub id: u64,
+    #[serde(rename = "userPresence")]
+    pub presence: UserPresence,
+}
+
 async fn generic_request<'a, R: Serialize, T: DeserializeOwned>(
     client: &mut Client,
     method: Method,
     path: &str,
     request: Option<&'a R>,
+    query: Option<&'a [(&'a str, &'a str)]>,
 ) -> Result<T, Error> {
     let mut builder = client
         .requestor
@@ -29,6 +134,10 @@ async fn generic_request<'a, R: Serialize, T: DeserializeOwned>(
     // Even though sending None works, it might get serialized as null in json, which is a waste of bytes
     if let Some(request) = request {
         builder = builder.json(&request);
+    }
+
+    if let Some(query) = query {
+        builder = builder.query(&query);
     }
 
     let response = client.validate_response(builder.send().await).await?;
@@ -42,7 +151,7 @@ async fn generic_count(client: &mut Client, path: &str) -> Result<u16, Error> {
     }
 
     Ok(
-        generic_request::<(), Response>(client, Method::GET, &format!("{path}/count"), None)
+        generic_request::<(), Response>(client, Method::GET, &format!("{path}/count"), None, None)
             .await?
             .count,
     )
@@ -89,16 +198,133 @@ pub async fn following_status(
         Method::POST,
         "user/following-exists",
         Some(&Request { user_ids: ids }),
+        None,
     )
     .await?
     .statuses)
 }
 
-// TODO:
-// add [GET] {URL}/my/friends/requests
-// add [GET] {URL}/users/{id}/friends/online
-// add [GET] {URL}/users/{id}/friends/statuses?userIds[]={id..}
-// add [GET] {URL}/users/{id}/followers
-// add [GET] {URL}/users/{id}/followings
-// add [GET] {URL}/users/{id}/friends/find
-// add [GET] {URL}/users/{id}/friends/search
+pub async fn friend_requests(
+    client: &mut Client,
+    paging: Paging<'_>,
+) -> Result<FriendRequests, Error> {
+    let limit = paging.limit.unwrap_or(18).to_string();
+    let cursor = paging.cursor.unwrap_or("");
+
+    generic_request::<(), FriendRequests>(
+        client,
+        Method::GET,
+        "my/friends/requests",
+        None,
+        Some(&[("cursor", cursor), ("limit", &limit)]),
+    )
+    .await
+}
+
+pub async fn user_followers(client: &mut Client, id: u64) -> Result<Followers, Error> {
+    generic_request::<(), Followers>(
+        client,
+        Method::GET,
+        &format!("users/{id}/followers"),
+        None,
+        None,
+    )
+    .await
+}
+
+pub async fn user_followings(client: &mut Client, id: u64) -> Result<Followers, Error> {
+    generic_request::<(), Followers>(
+        client,
+        Method::GET,
+        &format!("users/{id}/followings"),
+        None,
+        None,
+    )
+    .await
+}
+
+pub async fn user_friends_online(
+    client: &mut Client,
+    id: u64,
+) -> Result<Vec<FriendOnlineStatus>, Error> {
+    #[derive(Debug, Deserialize)]
+    struct Response {
+        #[serde(rename = "data")]
+        online: Vec<FriendOnlineStatus>,
+    }
+
+    Ok(generic_request::<(), Response>(
+        client,
+        Method::GET,
+        &format!("users/{id}/friends/online"),
+        None,
+        None,
+    )
+    .await?
+    .online)
+}
+
+pub async fn user_friends_find(
+    client: &mut Client,
+    id: u64,
+    paging: Paging<'_>,
+) -> Result<FriendsFind, Error> {
+    let limit = paging.limit.unwrap_or(18).to_string();
+    let cursor = paging.cursor.unwrap_or("");
+
+    generic_request::<(), FriendsFind>(
+        client,
+        Method::GET,
+        &format!("users/{id}/friends/find"),
+        None,
+        Some(&[("cursor", cursor), ("limit", &limit), ("userSort", "1")]),
+    )
+    .await
+}
+
+pub async fn user_friends_search(
+    client: &mut Client,
+    id: u64,
+    query: &str,
+    paging: Paging<'_>,
+) -> Result<FriendsFind, Error> {
+    let limit = paging.limit.unwrap_or(36).to_string();
+    let cursor = paging.cursor.unwrap_or("");
+
+    generic_request::<(), FriendsFind>(
+        client,
+        Method::GET,
+        &format!("users/{id}/friends/search"),
+        None,
+        Some(&[("cursor", &cursor), ("limit", &limit), ("query", query)]),
+    )
+    .await
+}
+
+pub async fn user_friend_statuses(
+    client: &mut Client,
+    id: u64,
+    friends: &[u64],
+) -> Result<Vec<FriendStatus>, Error> {
+    #[derive(Debug, Deserialize)]
+    struct Response {
+        #[serde(rename = "data")]
+        statuses: Vec<FriendStatus>,
+    }
+
+    let ids = friends
+        .iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join(",");
+
+    Ok(generic_request::<(), Response>(
+        client,
+        Method::GET,
+        &format!("users/{id}/friends/statuses"),
+        None,
+        Some(&[("userIds", &ids)]),
+    )
+    .await?
+    .statuses)
+}
--- tests/api/friends/v1.rs
+++ tests/api/friends/v1.rs
@@ -1,5 +1,5 @@
 use dotenvy_macro::dotenv;
-use roblox_api::{api::friends, client::Client};
+use roblox_api::{Paging, api::friends, client::Client};
 
 const USER_ID: u64 = 3139503587;
 
@@ -51,4 +51,62 @@ async fn following_status() {
     friends::v1::following_status(&mut client, &[USER_ID])
         .await
         .unwrap();
+}
+
+#[tokio::test]
+async fn friend_requests() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+    friends::v1::friend_requests(&mut client, Paging::default())
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn user_followers() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+    friends::v1::user_followers(&mut client, USER_ID)
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn user_followings() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+    friends::v1::user_followings(&mut client, USER_ID)
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn user_friends_online() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+    friends::v1::user_friends_online(&mut client, USER_ID)
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn user_friends_find() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+    friends::v1::user_friends_find(&mut client, USER_ID, Paging::default())
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn user_friends_search() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+    friends::v1::user_friends_search(&mut client, USER_ID, "Roblox", Paging::default())
+        .await
+        .unwrap();
+}
+
+#[tokio::test]
+async fn user_friend_statuses() {
+    let mut client = Client::from_cookie(dotenv!("ROBLOX_COOKIE").into());
+    let statuses = friends::v1::user_friend_statuses(&mut client, USER_ID, &[1])
+        .await
+        .unwrap();
+
+    assert_eq!(statuses.first().unwrap().id, 1);
 }
